8fa025f852c9cbc9a678cb8931ae40aeda78faeb
PeterGould <peter@olympiacircuits.com>
2020-01-11 09:42:23 -0800

    added map

---
+++ docs/graph.js
@@ -0,0 +1,174 @@
+var graph = false;
+var current_site = 'BUCKHORN2';
+var current_response = 'ht';
+var current_climate = 'mat';
+var datasets = false;
+
+    //create toolbar
+        $(function () {
+            $('#myToolbar').w2toolbar({
+                name : 'myToolbar',
+                items: [
+                    {type:"html",html:"<div>Response</div>"}
+                    ,{ type: 'menu',   id: 'response', caption: current_response, 
+                            items: [
+                            {text:"dbh"}
+                            ,{text:"ht"}
+                            ,{text:"surv"}
+                            ,{text: "vol"}
+                            ,{text:"volyld"}
+                            ]
+                        }
+                    ,{type:"html",html:"<div>Site</div>"}
+                    ,{ type: 'menu',   id: 'site', caption: current_site, 
+                        items: [
+                        {text:"BUCKHORN2"}
+                        ,{text:"DOORSTOP"}
+                        ,{text:"EVANS_CREEK"}
+                        ,{text: "FLORAS"}
+                        ,{text:"JAMMER3"}
+                        ,{text: "NORTONS"}
+                        ,{text:"SLICE_BUTTE"}
+                        ,{text: "SODA320"}
+                        ,{text:"STONE_NURSERY"}
+                        ]
+                    },
+                    {type:"html",html:"<div>Pop Climate</div>"},
+                    { type: 'menu',   id: 'climate', caption: current_climate, 
+                    items: [
+                    {text:"cmd"}
+                    ,{text:"emt"}
+                    ,{text:"eref"}
+                    ,{text:"map"}
+                    ,{text:"mat"}
+                    ,{text:"msp"}
+                    ,{text:"mcmt"}
+                    ,{text:"mwmt"}
+                    ,{text:"shm"}
+                    ,{text:"td"}
+                    ]
+                }
+                ],
+                onClick:function(event){
+                    if(event.subItem==null) return;
+                    process_click(event);
+                }
+            });
+        });
+        //process an event click
+        var process_click = function(event){
+            var pick = event.subItem.text;
+            event.item.text = pick;
+            if(event.item.id=="site") current_site = pick;
+            if(event.item.id=="response") current_response = pick;
+            if(event.item.id=="climate") current_climate = pick;
+            setup_graph();
+            w2ui.myToolbar.refresh();
+
+        }
+
+        //put together the data
+        var setup_graph = function(){
+            datasets = [];
+            //create by region
+            var region_sets = {};
+            //now parse data
+            var site_data = data[current_site];
+            Object.keys(site_data).forEach(
+                function(a_site){
+                    region_sets[a_site] = [];
+                    Object.keys(site_data[a_site]).forEach(
+                        function(pop_name){
+                            var a_pop =site_data[a_site][pop_name];                          
+                            var hover_message = ["Pop: " + a_pop.pop,"Region: " + a_pop.region, "DBH: " + a_pop.dbh10
+                                                ,"Ht: " + a_pop.ht10, "Surv: " + a_pop.surv10,
+                                                ,"Vol: " + a_pop.vol10, "VolYld: " + a_pop.volyld10
+                                                ,"Family 1: " + a_pop.fam1, " Elevation (m) " + a_pop.elev1
+                                                ,"Family 2: " + a_pop.fam2, " Elevation (m) " + a_pop.elev2
+                                            
+                                            ];
+                            var point = {
+                                x:a_pop["pop_" + current_climate]
+                                ,y:a_pop[current_response+'10']
+                                ,id:a_pop.pop
+                                ,pop:a_pop.pop
+                                ,fam1:a_pop.fam1
+                                ,fam2:a_pop.fam2
+                                ,hover_message:hover_message
+                            }; 
+                         region_sets[a_pop.region].push(point) ;     
+                        }
+                    );
+                        
+                });     
+            //add to datasets
+            Object.keys(region_sets).forEach(function(a_region){
+                var new_group = {
+                                    label:a_region,
+                                    pointBackgroundColor:region_colors[a_region].hex_color,
+                                    backgroundColor:region_colors[a_region].hex_color,
+                                    pointBorderColor:'rgba(0,0,0,1)',
+                                    pointRadius:4,
+                                    pointBorderWidth:1,
+                                    fill:false,
+                                    showLine:false,
+                                    data:region_sets[a_region]
+                                }
+                datasets.push(new_group);
+            })
+           publish_graph();
+        }
+
+        var publish_graph = function(){
+            var canvas = document.getElementById('graph_canvas');
+            var ctx = canvas.getContext('2d');
+            var graph_options  = {
+                type: 'scatter',
+                data: {
+                    datasets:datasets
+                },
+                options: {
+                    maintainAspectRatio:false
+                    ,scales: {
+                        xAxes: [{
+                            type: 'linear',
+                            position: 'bottom',
+                            scaleLabel: {
+                                display: true,
+                                labelString: current_climate
+                              }
+                            }]
+                        ,yAxes: [{
+                            type: 'linear',
+                            position: 'left',
+                            scaleLabel: {
+                                display: true,
+                                labelString: current_response
+                              }
+                        }]
+                    }
+                    ,tooltips: {
+                        callbacks: {
+                            label: function(tooltipItem, data) {
+                                return data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].hover_message;
+                            }
+                        }
+                    }
+                }
+            };
+            if(graph) graph.destroy();
+            graph = new Chart(ctx,graph_options); 
+            //setup on-click event
+            canvas.onclick = function(evt) {
+                var activePoints = graph.getElementAtEvent(evt);
+                if (activePoints[0]) {
+                  var chartData = activePoints[0]['_chart'].config.data;
+                  var datasetIndex = activePoints[0]['_datasetIndex']
+                  var idx = activePoints[0]['_index'];
+                  var point = datasets[datasetIndex].data[idx];
+                  PubSub.publish(POPULATION_FOCUS,[point.fam1,point.fam2]);
+                }
+              }
+
+        } 
+        
---
+++ docs/mapper.js
@@ -0,0 +1,152 @@
+
+var map = false;
+var point_grid = false;
+var point_layer = false;
+var BASELAYER_CHANGE = 'base_layer_change';
+var POPULATION_FOCUS = 'population_focus'
+var bases = { //basemap images
+    Imagery:{
+        url:'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+        ,attribution: '&copy; <a href="http://www.esri.com/">Esri</a>'
+        ,max_zoom:23
+    }
+   , Hillshade:{
+        url:'https://basemap.nationalmap.gov/arcgis/rest/services/USGSShadedReliefOnly/MapServer/tile/{z}/{y}/{x}'
+        ,attribution: '&copy; <a href="https://basemap.nationalmap.gov/arcgis/rest/services">USGS</a>'
+        ,max_zoom:16
+    }
+    , ImageTopo:{
+        url:'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}'
+        ,attribution: '&copy; <a href="https://basemap.nationalmap.gov/arcgis/rest/services">USGS</a>'
+        ,max_zoom:16
+    }
+    ,OpenTopo:{
+        url:'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
+        ,attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a>'
+        ,max_zoom:17
+    }
+    ,Street:{
+        url:'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        ,attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a>'
+        ,max_zoom:23
+    }
+    ,USGSClassic:{
+        url:'http://server.arcgisonline.com/arcgis/rest/services/USA_Topo_Maps/MapServer/tile/{z}/{y}/{x}'
+        ,attribution: '&copy; <a href="http://www.esri.com/">Esri</a>'
+        ,max_zoom:15
+    }
+    , USGSTopo:{
+        url:'https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}'
+        ,attribution: '&copy; <a href="https://basemap.nationalmap.gov/arcgis/rest/services">USGS</a>'
+        ,max_zoom:16
+    }
+}
+
+var show_blank_map = function(){
+    var map_holder = document.getElementById('map_div');
+    if(map){
+        map.remove();
+        map = false;
+    }
+    map = new L.map(map_holder,{zoomSnap:0.25,zoomDelta:0.25});
+    //create objects to add to map
+    
+    var pick_base = L.control({position: 'topleft'});
+    pick_base.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'baselayer_options');
+        div.innerHTML = '<select onchange="PubSub.publish(BASELAYER_CHANGE,this)">><option>USGSTopo</option><option>Imagery</option><option>Hillshade</option><option>ImageTopo</option><option>OpenTopo</option><option>Street</option><option>USGSClassic</option></select>';
+        div.firstChild.onmousedown = div.firstChild.ondblclick = L.DomEvent.stopPropagation;
+        return div;
+    }
+
+    //set imagery as default
+    baselayer = L.tileLayer(bases.USGSTopo.url, {
+        attribution: bases.USGSTopo.attribution
+        ,maxNativeZoom:bases.USGSTopo.max_zoom
+    });
+    //add scalebar
+    var sbar = L.control.scale({metric:true,imperial:false});
+    var map_callback = function(){   
+        //add objects now
+        pick_base.addTo(map);
+        sbar.addTo(map);
+        if(baselayer){
+            baselayer.addTo(map);
+        }
+        //change style
+        $('.leaflet-control-scale-line').css('background','rgba(255,255,255,1)');
+    };
+    map.whenReady(map_callback);
+    map.setView([46.1914,-122.1956],8);
+    
+}
+
+//show points
+var show_points = function () {
+    var plotColor = "#ff4230";
+    point_layer = null;
+    if(point_grid==null || point_grid.features.length==0) return;
+    point_layer = L.geoJson(point_grid, {
+        pointToLayer: function (feature, latlng) {
+            var pt_icon = L.divIcon({className:'leaflet-div-icon2',html:String(feature.family),iconAnchor:[20,30]});
+            return L.marker(latlng,{icon:pt_icon});
+        },
+    });
+point_layer.label = 'point_layer';
+   var pts = L.geoJson(point_grid, {
+        pointToLayer: function (feature, latlng) {
+             return new L.CircleMarker(latlng, {
+                        radius: 5
+                        ,weight:2
+                        ,fillColor:'red'
+                        ,fillOpacity:1
+                        ,color:'white'
+                        ,stroke:true
+             })
+        },
+    });
+     //make points clickable
+  /*  point_layer.on("click",function(e){
+        PubSub.publish(MAP_POINT_CLICK,{id:e.layer.feature.properties.id});
+    });
+*/
+    point_layer.addLayer(pts);
+    if(point_layer==null) return;
+    if(map){
+        map.eachLayer(function (layer) {
+            if(layer.label=='point_layer') map.removeLayer(layer);
+        });
+        point_layer.addTo(map);
+        //zoom to layer
+        if(point_layer.getLayers().length>0) map.fitBounds(point_layer.getBounds(),{maxZoom:12,padding:[50,50]});
+    }
+};
+
+PubSub.subscribe(BASELAYER_CHANGE,function(msg,data){
+    if(baselayer) baselayer.remove();
+    var new_base = data.value;
+    baselayer = L.tileLayer(bases[new_base].url, {
+        attribution: bases[new_base].attribution
+        ,maxNativeZoom:bases[new_base].max_zoom
+    });
+    baselayer.addTo(map);
+});
+
+PubSub.subscribe(POPULATION_FOCUS,function(msd,data){
+    point_grid = null;
+    var pt_array = [];
+    for(var k = 0; k<data.length;k++){
+        var a_fam = locations[data[k]]
+        var x = a_fam.lon
+        var y = a_fam.lat
+        var id = String(a_fam.family);
+        pt_array.push(turf.point([x,y],{name:id,id:id}));
+    } 
+    point_grid = turf.featureCollection(pt_array);
+    turf.featureEach(point_grid,function(feature,index){
+        feature.family = feature.properties.name;
+    });
+    show_points();
+
+});
+
